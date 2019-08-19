6ee4f5c42ac5ee71f781c63488bfed4dc88d2e46
yubel26 <yubelribot26@gmail.com>
2019-08-18 20:41:32 -0400

    fixed the firebase database

--- README.md
+++ README.md
@@ -1,16 +1,28 @@
-# Train Scheduler
+# Train Schedule
 
-Instructions: 
+### Overview
 
-In this assignment, you'll create a train schedule application that incorporates Firebase to host arrival and departure data. Your app will retrieve and manipulate this information with Moment.js. This website will provide up-to-date information about various trains, namely their arrival times and how many minutes remain until they arrive at their station.
+In this assignment, the objective was to create a train schedule application that incorporates Firebase to host arrival and departure data. My app retrieves and manipulates the data with Moment.js. This website provides up-to-date information about various trains, namely their arrival times and how many minutes remain until they arrive at their station.
+
+### Requirements
+
+* App must suit these basic specs:
+  * When adding trains, administrators should be able to submit the following:
+    * Train Name
+    * Destination 
+    * First Train Time -- in military time
+    * Frequency -- in minutes
+  * Calculate when the next train will arrive; relative to the current time.
+  * Users from many different machines must be able to view same train times.
+
+### Added Functionality
+
+* Form Validation for correct time input
 
 
-### Built With
+### Link
+[Train-Schedule](https://ricopella.github.io/train-schedule/)
 
-* HTML5
-* CSS3
-* JavaScript
-* Bootstrap Framework
-* jQuery 
-* Firebase Database
-* Moment.js
+### Example Site
+
+![Train-Schedule](assets/images/train-schedule.png)
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,73 +1,142 @@
-$(document).ready(function(){
-    var config = {
-        apiKey: "AIzaSyCQ27pN1pHuW6sh8HYM20ucVJDAdMktfA0",
-        authDomain: "trainstation-c09db.firebaseapp.com",
-        databaseURL: "https://trainstation-c09db.firebaseio.com",
-        projectId: "trainstation-c09db",
-        storageBucket: "gs://trainstation-c09db.appspot.com",
-        messagingSenderId: "128486188002",
-        appId: "1:128486188002:web:71b45ac8b1345d5d"
-    };
-    firebase.initializeApp(config);
+////////////////////////////////////////////////////////
+////////////////////  TRAIN        ////////////////////
+///////////////////      SCHEDULE ////////////////////
+/////////////////////////////////////////////////////
 
-    // A variable to reference the database.
-    var database = firebase.database();
 
-    // Variables for the onClick event
-    var name;
-    var destination;
-    var firstTrain;
-    var frequency = 0;
 
-    $("#add-train").on("click", function() {
-        event.preventDefault();
-        // Storing and retreiving new train data
-        name = $("#train-name").val().trim();
-        destination = $("#destination").val().trim();
-        firstTrain = $("#first-train").val().trim();
-        frequency = $("#frequency").val().trim();
+// Global Variables
+var trainName = "";
+var trainDestination = "";
+var trainTime = "";
+var trainFrequency = "";
+var nextArrival = "";
+var minutesAway = "";
 
-        // Pushing to database
-        database.ref().push({
-            name: name,
-            destination: destination,
-            firstTrain: firstTrain,
-            frequency: frequency,
-            dateAdded: firebase.database.ServerValue.TIMESTAMP
+// jQuery global variables
+var elTrain = $("#train-name");
+var elTrainDestination = $("#train-destination");
+// form validation for Time using jQuery Mask plugin
+var elTrainTime = $("#train-time").mask("00:00");
+var elTimeFreq = $("#time-freq").mask("00");
+
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDRc8WHqEiPmt5eZgB_HxRygGA31dqHKPg",
+    authDomain: "train-schedule-bd950.firebaseapp.com",
+    databaseURL: "https://train-schedule-bd950.firebaseio.com",
+    projectId: "train-schedule-bd950",
+    storageBucket: "train-schedule-bd950.appspot.com",
+    messagingSenderId: "236709814864"
+};
+
+firebase.initializeApp(config);
+
+// Assign the reference to the database to a variable named 'database'
+var database = firebase.database();
+
+database.ref("/trains").on("child_added", function(snapshot) {
+
+    //  create local variables to store the data from firebase
+    var trainDiff = 0;
+    var trainRemainder = 0;
+    var minutesTillArrival = "";
+    var nextTrainTime = "";
+    var frequency = snapshot.val().frequency;
+
+    // compute the difference in time from 'now' and the first train using UNIX timestamp, store in var and convert to minutes
+    trainDiff = moment().diff(moment.unix(snapshot.val().time), "minutes");
+
+    // get the remainder of time by using 'moderator' with the frequency & time difference, store in var
+    trainRemainder = trainDiff % frequency;
+
+    // subtract the remainder from the frequency, store in var
+    minutesTillArrival = frequency - trainRemainder;
+
+    // add minutesTillArrival to now, to find next train & convert to standard time format
+    nextTrainTime = moment().add(minutesTillArrival, "m").format("hh:mm A");
+
+    // append to our table of trains, inside tbody, with a new row of the train data
+    $("#table-data").append(
+        "<tr><td>" + snapshot.val().name + "</td>" +
+        "<td>" + snapshot.val().destination + "</td>" +
+        "<td>" + frequency + "</td>" +
+        "<td>" + minutesTillArrival + "</td>" +
+        "<td>" + nextTrainTime + "  " + "<a><span class='glyphicon glyphicon-remove icon-hidden' aria-hidden='true'></span></a>" + "</td></tr>"
+    );
+
+    $("span").hide();
+
+    // Hover view of delete button
+    $("tr").hover(
+        function() {
+            $(this).find("span").show();
+        },
+        function() {
+            $(this).find("span").hide();
         });
-        $("form")[0].reset();
+
+    // STARTED BONUS TO REMOVE ITEMS ** not finished **
+    $("#table-data").on("click", "tr span", function() {
+        console.log(this);
+        var trainRef = database.ref("/trains/");
+        console.log(trainRef);
+    });
+});
+
+// function to call the button event, and store the values in the input form
+var storeInputs = function(event) {
+    // prevent from from reseting
+    event.preventDefault();
+
+    // get & store input values
+    trainName = elTrain.val().trim();
+    trainDestination = elTrainDestination.val().trim();
+    trainTime = moment(elTrainTime.val().trim(), "HH:mm").subtract(1, "years").format("X");
+    trainFrequency = elTimeFreq.val().trim();
+
+    // add to firebase databse
+    database.ref("/trains").push({
+        name: trainName,
+        destination: trainDestination,
+        time: trainTime,
+        frequency: trainFrequency,
+        nextArrival: nextArrival,
+        minutesAway: minutesAway,
+        date_added: firebase.database.ServerValue.TIMESTAMP
     });
 
-    database.ref().on("child_added", function(childSnapshot) {
-        var nextArr;
-        var minAway;
-        // Chang year so first train comes before now
-        var firstTrainNew = moment(childSnapshot.val().firstTrain, "hh:mm").subtract(1, "years");
-        // Difference between the current and firstTrain
-        var diffTime = moment().diff(moment(firstTrainNew), "minutes");
-        var remainder = diffTime % childSnapshot.val().frequency;
-        // Minutes until next train
-        var minAway = childSnapshot.val().frequency - remainder;
-        // Next train time
-        var nextTrain = moment().add(minAway, "minutes");
-        nextTrain = moment(nextTrain).format("hh:mm");
+    //  alert that train was added
+    alert("Train successuflly added!");
 
-        $("#add-row").append("<tr><td>" + childSnapshot.val().name +
-                "</td><td>" + childSnapshot.val().destination +
-                "</td><td>" + childSnapshot.val().frequency +
-                "</td><td>" + nextTrain + 
-                "</td><td>" + minAway + "</td></tr>");
+    //  empty form once submitted
+    elTrain.val("");
+    elTrainDestination.val("");
+    elTrainTime.val("");
+    elTimeFreq.val("");
+};
 
-            // Handle the errors
-        }, function(errorObject) {
-            console.log("Errors handled: " + errorObject.code);
-    });
+// Calls storeInputs function if submit button clicked
+$("#btn-add").on("click", function(event) {
+    // form validation - if empty - alert
+    if (elTrain.val().length === 0 || elTrainDestination.val().length === 0 || elTrainTime.val().length === 0 || elTimeFreq === 0) {
+        alert("Please Fill All Required Fields");
+    } else {
+        // if form is filled out, run function
+        storeInputs(event);
+    }
+});
 
-    database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
-        // Change the HTML to reflect
-        $("#name-display").html(snapshot.val().name);
-        $("#email-display").html(snapshot.val().email);
-        $("#age-display").html(snapshot.val().age);
-        $("#comment-display").html(snapshot.val().comment);
-    });
+// Calls storeInputs function if enter key is clicked
+$('form').on("keypress", function(event) {
+    if (event.which === 13) {
+        // form validation - if empty - alert
+        if (elTrain.val().length === 0 || elTrainDestination.val().length === 0 || elTrainTime.val().length === 0 || elTimeFreq === 0) {
+            alert("Please Fill All Required Fields");
+        } else {
+            // if form is filled out, run function
+            storeInputs(event);
+        }
+    }
 });
